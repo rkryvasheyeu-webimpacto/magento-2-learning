6170557829bc710212817014a8adf531c24e4ab2
Raman-wi <rkryvasheyeu@webimpacto.es>
2019-12-19 17:22:43 +0300

    feat: Manage Resource Model.

---
+++ app/code/Mastering/SampleModule/Model/ResourceModel/Item/Collection.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mastering\SampleModule\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+    protected $_idFieldName = 'id';
+
+    protected function _construct()
+    {
+        $this->_init(
+            \Mastering\SampleModule\Model\Item::class,
+            Item::class
+        );
+    }
+}
